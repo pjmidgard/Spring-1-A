0d04a3a81a89a8219166dafc3006c4d5bd5c1ace
Java Software <pj.midgard@yahoo.com>
2022-09-24 12:13:20 +0100

    Create Spring-1-A.py

--- Spring-1-A.py
+++ Spring-1-A.py
@@ -209,10 +209,10 @@ class compression:
                                         Number=int(ILIN,2)
                                         Str_Ilin_Number_Save=str(Number) 
                                         long5=len(Str_Ilin_Number_Save)
-                                        if str(Number)[0:1]!="1" and str(Number)[0:1]!="2" and str(Number)[0:1]!="3" and str(Number)[0:1]!="4" and long5==4:
+                                        if str(Number)[0:1]!="1" and str(Number)[0:1]!="2" and str(Number)[0:1]!="3" and long5==4:
                                             str1=""
                                             
-                                        elif long5<=4:
+                                        elif long5<=3:
                                             str1=str(long5)
                                         #print(str1+Str_Ilin_Number_Save)
                                         Number_Save=Number_Save+str1+Str_Ilin_Number_Save
